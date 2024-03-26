470d9969191b34c7f851caefb8f267f42ac4ee52
ayoshinin <ayodejisamson73@yahoo.com>
2024-03-25 21:23:12 -0700

    binary function

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,12 @@
+
+#include "binary_trees.h"
+/**
+ * binary_tree_depth -It depth of specified node from root
+ * @tree:  check the depth of node
+ * Return: 0 is it is the root or number of depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
+
---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,22 @@
+
+i#include "binary_trees.h"
+/**
+ * binary_tree_inorder - print elements of tree using in-order traversal
+ * @tree: Going through the tree
+ * @func: functional used tree
+ * Return: Nothing
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	else
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+}
+
